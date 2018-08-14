d516861771e17d08d0c0da97412ba28e3f2e57c9
unknown <nguyennam5197@gmail.com>
2018-08-14 15:26:56 +0700

    Removed node_modules

---
+++ ws.js
@@ -0,0 +1,12 @@
+
+var WebSocketServer = require('ws').Server,
+wss = new WebSocketServer({port: 40510})
+wss.on('connection', function (ws) {
+ws.on('message', function (message) {
+  console.log('received: %s', message)
+})
+setInterval(
+  () => ws.send(`${new Date()}`),
+  1000
+)
+})
