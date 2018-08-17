5fcd0ad4a02b2bbe126f5458d2588d9a15d49dd3
unknown <nguyennam5197@gmail.com>
2018-08-18 01:10:15 +0700

    ahihi2

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ var bodyParser = require('body-parser')
 var WebSocketServer = require('ws').Server;
 var request = require('request');
 const path = require('path');
-var randomstring = require("randomstring");
-var app = express();
+const uuid = require('uuid');
+const session = require('express-session');
 
+
+var app = express();
 const PORT = process.env.PORT || 80;
 
 app.set('view engine', 'hbs')
@@ -15,125 +17,133 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 hbs.registerPartials(__dirname + '/views/partials');
 
-const randne = randomstring.generate();
-
-wss = new WebSocketServer({port: 40510});
-
-function noop() {}
-
-function heartbeat() {
-  this.isAlive = true;
-}
-
-wss.on('connection', function connection(ws) {
-  ws.isAlive = true;
-  ws.on('pong', heartbeat);
+const sessionParser = session({
+    saveUninitialized: false,
+    secret: '$eCuRiTy',
+    resave: false
 });
-
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
-}, 30000);
-
-app.use((req, res, next) => {
-    req.wss = wss
-    next()
-})
-
+app.use(sessionParser);
+//wss = new WebSocketServer({port: 40510});
+app.enable('trust proxy')
 
 app.get("/", (req,res) => {
     res.render('home.hbs');
+    const id = uuid.v4();
+    console.log(`Updating session for user ${id}`);
+    req.session.userId = id;
+    //res.send({ result: 'OK', message: 'Session updated' });
 })
+
+const wss = new WebSocketServer({
+    port: 40510,
+    verifyClient: (info, done) => {
+      console.log('Parsing session from request...');
+      sessionParser(info.req, {}, () => {
+        console.log('Session is parsed!');
+        done(info.req.session.userId);
+      });
+    },
+    app
+});
+
+wss.on('connection', (ws, req) => {
+    //ws.send(req.session.userId)
+    ws.on('message', (message) => {
+        ws.userId = req.session.userId;
+    });
+});
 
 app.post('/nampro', (req,res) => {
     var token = req.body.token2;
-    var idg = req.body.idg;
+    var idg = req.body.idg; 
 
-    // request({
-    //     uri: `https://graph.facebook.com/${idg}/members?access_token=${token}&format=json&method=get&summary=true&limit=false`,
-    //     method: 'GET',
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Accept-Charset': 'utf-8',
-    //     }
-
-    // }, (error, response, body) => {
-    //    if(error) {
-    //        console.log("loi roi");
-    //    }
-    //    var total = JSON.parse(body).summary;
-    //    console.log
-    // });
-
-    wss.on('connection', function (ws) {
-
-        ws.on('message', function (message) {
-            console.log('received: %s', message)
-        })
-       
+    var ahihia = req.session.userId;
+    console.log(ahihia);
         var options = {
-            uri: `https://graph.facebook.com/${idg}/members?fields=id&limit=5000&access_token=${token}`,
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Accept-Charset': 'utf-8',
-            }
+        uri: `https://graph.facebook.com/${idg}/members?fields=id&limit=5000&access_token=${token}`,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
         }
-        function callback(error, response, body) {
-            if (!error) {
-                var info = JSON.parse(body).data;
-                if (info != "undefined" || info !="null") {var ahihi = Object.keys(info).length;}
-                else{console.log("error")}
-                
-                for ( var i=0; i<info.length;i++) {
-                    var d = info[i];
-                    var t = d.id + '\r\n';
-                    try {
-                        fs.appendFileSync(__dirname + "/public/"+randne+".txt", t);
-                    } catch (err) {
-                        if (err) throw err;
-                    }
-                }
-                if (ahihi == i) {
-                    var n = JSON.parse(body).paging;
-                    var a = n.next;
-                    if(typeof a != 'undefined'){
-                        const options = {  
-                            uri: a,
-                            method: 'GET',
-                            headers: {
-                                'Accept': 'application/json',
-                                'Accept-Charset': 'utf-8',
-                            }
-                        };
-                        request(options, callback);
-                    } else {
-                        fs.readFile(__dirname + "/public/"+randne+".txt", "utf8", function read(err, data) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                 
-                                ws.send(randne);
-                                ws.close();
-                            }
-                        });
-                    } 
-                }else{
-                    console.log("ahihi")
+    }
+    function callback(error, response, body) {
+        if (!error) {
+            var info = JSON.parse(body).data;
+            if (info != "undefined" || info !="null") {var ahihi = Object.keys(info).length;}
+            else{console.log("error")}
+            
+            for ( var i=0; i<info.length;i++) {
+                var d = info[i];
+                var t = d.id + '\r\n';
+                try {
+                    fs.appendFileSync(__dirname + "/public/"+ahihia+".txt", t);
+                } catch (err) {
+                    if (err) throw err;
                 }
             }
-            return error;
+            if (ahihi == i) {
+                var n = JSON.parse(body).paging;
+                var a = n.next;
+                if(typeof a != 'undefined'){
+                    const options = {  
+                        uri: a,
+                        method: 'GET',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Accept-Charset': 'utf-8',
+                        }
+                    };
+                    request(options, callback);
+                } else {
+                    fs.readFile(__dirname + "/public/"+ahihia+".txt", "utf8", function read(err, data) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            req.wss.    
+                        }
+                    });
+                } 
+            }else{
+                console.log("ahihi")
+            }
         }
-        request(options, callback);
-    }); 
+        return error;
+    }
+    request(options, callback);
 
     res.render('result.hbs');
 })
 
+
+
+
+
+
+// function noop() {}
+
+// function heartbeat() {
+//   this.isAlive = true;
+// }
+// wss.on('connection', function (ws) {
+
+//     ws.send(JSON.stringify({
+//         type: 'initialization',
+//         id: token
+//     }))
+
+
+//     ws.isAlive = true
+//     ws.on('pong', heartbeat)
+
+//     ws.on('message', (message) => {
+//         console.log("ahih", message)
+//     })
+    
+//     ws.on('error', () => {
+//         console.log('error webcoket')
+//     })
+// });
 
 app.get("/download/:file(*)", (req,res) => {
     var file = req.params.file;
